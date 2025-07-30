d65d24f260b85b57e8c4a6161a771348c591be55
ParsaImi <parsaemani17@gmail.com>
2025-07-30 22:14:48 +0330

    IPs changed

--- frontend/elevenfingers/src/routes/login/+page.svelte
+++ frontend/elevenfingers/src/routes/login/+page.svelte
@@ -32,7 +32,7 @@
     isLoading = true;
     
     try {
-      const response = await fetch('http://localhost:8000/auth/login', {
+      const response = await fetch('http://160.191.77.176:8000/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
--- websocket/cmd/main.go
+++ websocket/cmd/main.go
@@ -23,29 +23,6 @@ type AuthResponse struct {
 	UserName string
 }
 
-func verifyTokenWithFastAPI(token string) (*AuthResponse, error) {
-	authReq := AuthRequest{
-		Scheme: "bearer",
-		Credentials: token,
-	}
-	jsonData, err := json.Marshal(authReq)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := http.Post(
-		"http://127.0.0.1:8000/verify",
-		"application/json",
-		bytes.NewBuffer(jsonData),
-	)
-	if err != nil {
-		return nil , err
-	}
-	var result AuthResponse
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return nil,err
-	}
-	return &result, nil
-}
 
 
 func HealthCheck(w http.ResponseWriter , r *http.Request){
